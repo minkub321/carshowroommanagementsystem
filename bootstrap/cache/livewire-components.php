a5c34b920f9f178747979e621afdd07cec2c0d43
minkub321 <pattarapon9473@gmail.com>
2025-03-22 02:14:17 +0700

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'admin-footer' => 'App\\Http\\Livewire\\AdminFooter',
+  'admin-navbar' => 'App\\Http\\Livewire\\AdminNavbar',
+  'admin-sidebar' => 'App\\Http\\Livewire\\AdminSidebar',
+  'car-slider' => 'App\\Http\\Livewire\\CarSlider',
+  'footer' => 'App\\Http\\Livewire\\Footer',
+  'header' => 'App\\Http\\Livewire\\Header',
+);
