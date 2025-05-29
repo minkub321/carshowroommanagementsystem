c28a340a2ecb0d71f5a7ccb153a9cc94cbd90f87
minkub321 <pattarapon9473@gmail.com>
2025-05-29 09:56:38 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\HomeCarController;
-use App\View\Components\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,26 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('components.home');
-})->name('home');
-
-Route::resource('/cars', HomeCarController::class)->only(['index', 'show']);
-
-Route::get('/contact-us', [ContactController::class, 'index'])->name('contact-us');
-Route::post('/contact-us', [ContactController::class, 'contactPost'])->name('contact-post');
-
-Route::get('/about-us', function(){
-    return view('components.about-us');
-})->name('about-us');
-
-Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::resource('/car', CarController::class)->except(['show']);
+    return view('welcome');
 });
